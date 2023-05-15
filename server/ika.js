05a83ea358b00ea66a66178f9700030054c72983
Yellow Machine <yellow.machine@mailfence.com>
2023-05-15 17:57:05 +0200

    adding emitter

--- server/ika.js
+++ server/ika.js
@@ -1,6 +1,28 @@
 const { readdir, readFile, writeFile } = require('fs/promises')
 const { v2: compose } = require('docker-compose')
 const path = require('path')
+const DockerEvents = require("docker-events")
+const Dockerode = require('dockerode');
+
+var emitter = new DockerEvents({
+    docker: new Dockerode({socketPath: '/var/run/docker.sock'}),
+});
+
+function startEmitter(){
+    emitter.start();
+
+    emitter.on("connect", function() {
+        console.log("connected to docker api");
+    });
+
+    emitter.on("disconnect", function() {
+        console.log("disconnected to docker api; reconnecting");
+    });
+
+    emitter.on("_message", function(message) {
+        console.log("got a message from docker: %j", message);
+    });
+}
 
 async function cmd(cmd, workspace, options){
     try{
@@ -50,12 +72,12 @@ async function writeReadme(name, txt){
     await writeFile(`${root}/${name}/README`, txt, "utf8")
 }
 
-async function up(workspace){
+async function upWorkspace(workspace){
     await cmd('upAll', workspace)
 }
 
 
-async function down(workspace){
+async function downWorkspace(workspace){
     await cmd('down', workspace)
 }
 

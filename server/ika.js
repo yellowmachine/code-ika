1388cfbc707923eee4c1f314484b32b67faebc8e
Yellow Machine <yellow.machine@mailfence.com>
2023-05-15 18:21:09 +0200

    magage workspaces

--- server/ika.js
+++ server/ika.js
@@ -1,14 +1,14 @@
-const { readdir, readFile, writeFile } = require('fs/promises')
+const { readdir, readFile, writeFile, stat, rm } = require('fs/promises')
 const { v2: compose } = require('docker-compose')
 const path = require('path')
 const DockerEvents = require("docker-events")
 const Dockerode = require('dockerode');
 
-var emitter = new DockerEvents({
-    docker: new Dockerode({socketPath: '/var/run/docker.sock'}),
-});
-
 function startEmitter(){
+    const emitter = new DockerEvents({
+        docker: new Dockerode({socketPath: '/var/run/docker.sock'}),
+    });
+
     emitter.start();
 
     emitter.on("connect", function() {
@@ -22,6 +22,8 @@ function startEmitter(){
     emitter.on("_message", function(message) {
         console.log("got a message from docker: %j", message);
     });
+
+    return emitter.stop
 }
 
 async function cmd(cmd, workspace, options){
@@ -63,27 +65,69 @@ async function getStates(){
     return obj
 }
 
+async function getWorkspace(name){
+    const readme = readReadme(name)
+    const specification = readSpecification(name)
+    return {readme, specification}
+}
+
 async function readReadme(name){
     return await readFile(`${root}/${name}/README`, "utf8")
 }
 
+async function readSpecification(name){
+    return await readFile(`${root}/${name}/docker-compose.yml`, "utf8")
+}
+
+async function write(dest, txt){
+    await writeFile(`${dest}`, txt, "utf8")
+}
 
 async function writeReadme(name, txt){
-    await writeFile(`${root}/${name}/README`, txt, "utf8")
+    await write(`${root}/${name}/README`, txt)
+}
+
+async function writeSpecification(name, txt){
+    await write(`${root}/${name}/docker-compose.yml`, txt)
+}
+
+async function isWorkspace(name){
+    const err = await stat(`${root}/${name}`)
+    if(!err) return true
+    else return false
 }
 
 async function upWorkspace(workspace){
     await cmd('upAll', workspace)
 }
 
-
 async function downWorkspace(workspace){
     await cmd('down', workspace)
 }
 
+async function createWorkspace(name, specification, readme){
+    if(isWorkspace(name)) throw "Workspace already exists"
+    await writeReadme(name, readme)
+    await writeSpecification(name, readme)
+}
+
+async function updateWorkspace(name, specification, readme){
+    if(!isWorkspace(name)) throw "Workspace doesn't exist"
+    await writeReadme(name, readme)
+    await writeSpecification(name, readme)
+}
+
+async function deleteWorkspace(name){
+    if(!isWorkspace(name)) throw "Workspace doesn't exist"
+    await downWorkspace(name)
+    await rm(`${root}/${name}`, { recursive: true, force: true });
+}
+
 async function main(){
+    const stop = startEmitter()
     const x = await getStates()
     console.log(x)
+    stop()
 }
 
 main()

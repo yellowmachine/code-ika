69300a6e6d128486c43c8a1804f8cdb3a166d66f
Yellow Machine <yellow.machine@mailfence.com>
2023-05-15 17:38:08 +0200

    Readme in workspacess

--- server/index.js
+++ server/index.js
@@ -1,23 +1,55 @@
+const { readdir, readFile, writeFile } = require('fs/promises')
 const { v2: compose } = require('docker-compose')
 const path = require('path')
 
-
-async function cmd(cmd, options){
+async function cmd(cmd, source, options){
     try{
         const res = await compose[cmd]({
-            cwd: path.join(__dirname),
+            cwd: path.join(source),
             commandOptions: options
         })
-        console.log(res.data)
+        return res
     }
     catch(err){
         console.log(err)
     }
 }
 
-async function main(){
-    await cmd('upAll')
-    await cmd('ps')
+const getDirectories = async source =>
+  (await readdir(source, { withFileTypes: true }))
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
+
+const directoryState = async (source) => {
+    return (await cmd('ps', source)).data
+}
+
+const root = 'workspaces'
+
+async function main(){    
+    const dirs = await getDirectories(root)
+    
+    const states = await Promise.all(
+        dirs.map(async (name) => {
+            return await directoryState(`${root}/${name}`)
+        })
+    )
+    
+    var obj = {};
+    dirs.forEach((key, i) => obj[key] = states[i]);
+    console.log(obj);
+}
+
+async function readReadme(name){
+    const data = await readFile(`${root}/${name}/README`, "utf8")
+    console.log(data)
+}
+
+
+async function writeReadme(name, txt){
+    await writeFile(`${root}/${name}/README`, txt)
 }
 
 main()
+readReadme('ika')
+writeReadme('puf', 'hola ;)')
